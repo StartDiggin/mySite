ad2a0523c2f47b7e8e1128101f09525bfa1a5e40
Johnny Gonzales <dimensions122@gmail.com>
2019-01-28 16:16:08 -0500

    adding images

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,15 @@ import React from 'react'
 class Home extends React.Component {
     render() {
         return (
+            <>
             <div id="home">
-                <h1>Home</h1>
+                <h1>I Help Small Businesses</h1>
+                <h2>Get More Customers</h2>
             </div>
+            <div className="overlay">
+
+            </div>
+            </>
         ) 
     }
 };
